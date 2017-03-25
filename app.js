c7dbdef06fc1a3311efc7289d5191cb6ea53a4ba
Julie Hoffman <jhoffman2013@gmail.com>
2017-03-25 01:06:28 -0400

    Add Folders and Update File Content

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
+var mongo = require('mongodb');
+
+var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
@@ -14,7 +16,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
+// Remember to uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
